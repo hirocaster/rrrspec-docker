04fc547a9609d8146b95f2c9f29c8634983305b4
hirocaster <hohtsuka@gmail.com>
2016-05-27 21:59:12 +0900

    Add --no-cache option in build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+usage_exit() {
+        echo "Usage: $0 [-n]" 1>&2
+        exit 1
+}
+
+while getopts nh OPT
+do
+    case $OPT in
+        n)  NO_CACHE="--no-cache=true"
+            ;;
+        h)  usage_exit
+            ;;
+        \?)
+            ;;
+    esac
+done
+
+shift $((OPTIND - 1))
 
 CONTAINER_DIR=$(cd $(dirname $0) && pwd)/containers
 
 docker build -t rrrspec/redis ${CONTAINER_DIR}/redis
 docker build -t rrrspec/mysql ${CONTAINER_DIR}/mysql
 docker build -t rrrspec/rsyncd ${CONTAINER_DIR}/rsyncd
-docker build -t rrrspec/rrrspec-base ${CONTAINER_DIR}/rrrspec-base
-docker build -t rrrspec/rrrspec-server ${CONTAINER_DIR}/rrrspec-server
-docker build -t rrrspec/rrrspec-worker ${CONTAINER_DIR}/rrrspec-worker
-docker build -t rrrspec/rrrspec-client ${CONTAINER_DIR}/rrrspec-client
+docker build ${NO_CACHE} -t rrrspec/rrrspec-base ${CONTAINER_DIR}/rrrspec-base
+docker build ${NO_CACHE} -t rrrspec/rrrspec-server ${CONTAINER_DIR}/rrrspec-server
+docker build ${NO_CACHE} -t rrrspec/rrrspec-worker ${CONTAINER_DIR}/rrrspec-worker
+docker build ${NO_CACHE} -t rrrspec/rrrspec-client ${CONTAINER_DIR}/rrrspec-client
