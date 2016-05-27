b9ca82eab9cc2c82b06258b5987b334600c562ce
hirocaster <hohtsuka@gmail.com>
2016-05-27 21:59:47 +0900

    Change rrrspec-client on bundle

--- containers/rrrspec-client/run.sh
+++ containers/rrrspec-client/run.sh
@@ -4,6 +4,8 @@ set -e
 
 cd /opt/project
 
-TASK_KEY=$(rrrspec-client start --config /opt/lib/rrrspec-server-config.rb --rsync-name "${USER}" --key-only)
-rrrspec-client waitfor $TASK_KEY --config /opt/lib/rrrspec-server-config.rb --pollsec=10
-exec rrrspec-client show $TASK_KEY --config /opt/lib/rrrspec-server-config.rb --verbose --failure-exit-code=1
+bundle install
+
+TASK_KEY=$(bundle exec rrrspec-client start --config /opt/lib/rrrspec-server-config.rb --rsync-name "${USER}" --key-only)
+bundle exec rrrspec-client waitfor $TASK_KEY --config /opt/lib/rrrspec-server-config.rb --pollsec=10
+exec bundle exec rrrspec-client show $TASK_KEY --config /opt/lib/rrrspec-server-config.rb --verbose --failure-exit-code=1
