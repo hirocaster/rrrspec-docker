7c61380a959978cdaba57cf4e8dfc6a20d629cbf
hirocaster <hohtsuka@gmail.com>
2016-05-31 14:14:37 +0900

    Split client config file

--- containers/rrrspec-client/run.sh
+++ containers/rrrspec-client/run.sh
@@ -6,6 +6,6 @@ cd /opt/project
 
 bundle install
 
-TASK_KEY=$(bundle exec rrrspec-client start --config /opt/lib/rrrspec-server-config.rb --rsync-name "${USER}" --key-only)
-bundle exec rrrspec-client waitfor $TASK_KEY --config /opt/lib/rrrspec-server-config.rb --pollsec=10
-exec bundle exec rrrspec-client show $TASK_KEY --config /opt/lib/rrrspec-server-config.rb --verbose --failure-exit-code=1
+TASK_KEY=$(bundle exec rrrspec-client start --rsync-name "${USER}" --key-only)
+bundle exec rrrspec-client waitfor $TASK_KEY --pollsec=10
+exec bundle exec rrrspec-client show $TASK_KEY --verbose --failure-exit-code=1
